8bbb271d5645fe4d6d7a665f6a9bd74177748b5c
Keith Gardner <wkeithgardner@gmail.com>
2025-11-30 20:07:01 -0500

    Added composer autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'asolution-company/ai-summaries',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '59ae54292d3b56f8586935b2725f355e9eaf5b61',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'asolution-company/ai-summaries' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '59ae54292d3b56f8586935b2725f355e9eaf5b61',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
